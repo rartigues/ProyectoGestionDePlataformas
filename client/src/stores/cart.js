d5a9c70866c6a689ae0f5e1984e4efb093b16dc2
rartigues2019 <rartigues2019@inf.udec.cl>
2022-12-05 06:19:51 -0300

    Implemented buy cart

--- client/src/stores/cart.js
+++ client/src/stores/cart.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-labels */
 import { defineStore } from 'pinia'
 
 export const useCart = defineStore('cart-store', {
@@ -45,6 +46,45 @@ export const useCart = defineStore('cart-store', {
     addToCart(producto) {
       this.cart = this.cart.slice(0);
       this.cart.push(producto);
+    },
+    async makeCompra(contents) {
+      // console.log(`makeCompra()`);
+      // console.log(`Contents: ${JSON.stringify(contents)}`);
+      try {
+        // for every item in cart, create a LineaCompra. Copy getContents() way of doing it.
+        // map lineacompra productoId, cantidad, total
+        var lineasCompra =  contents.map(item => {
+          return {
+            productoId: item.id,
+            cantidad: item.quantity,
+            total: item.precio
+          }
+        });
+
+        // console.log(JSON.stringify(lineasCompra));
+        const response = await fetch('http://127.0.0.1:3000/api/compras', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            userId: 14,
+            total: this.total + 1500,
+            lineasCompra: lineasCompra
+          })
+        });
+        const JSONresponse = await response.json();
+        console.debug(`Compra response: ${JSON.stringify(JSONresponse)}`);
+        // empty cart
+        this.cart = [];
+        
+
+        console.debug(`Contenidos del carrito: ${JSON.stringify(this.cart)}`);
+      } catch (err) {
+        // this.productos = [];
+        console.error('Error loading productos:', err);
+        return
+      }
     }
   }
 })

138f6456bbfa8a65c5158e2483838aaa3e619ef9
rartigues2019 <rartigues2019@inf.udec.cl>
2022-12-05 05:19:00 -0300

    Added beta form for checkout

--- client/src/pages/cart/Cart.vue
+++ client/src/pages/cart/Cart.vue
@@ -2,12 +2,14 @@
 <script setup>
 import TopNavbar from '../../components/TopNavbar.vue';
 import BottonFooter from '../../components/BottonFooter.vue';
-import {useCart} from '../../stores/cart.js'
+import {useCart} from '../../stores/cart.js';
+
 const store = useCart()
 </script>
 
 
 <template src="./cart.html"></template>
+
 
 <script>
 import { mapState, mapActions } from 'pinia'
@@ -17,21 +19,38 @@ export default {
     BottonFooter
   },
   mounted() {
-    //console.log('mounted', this.contents)
+    
   },
   computed: {
     ...mapState(useCart, {
       contents: 'getContents',
       count: 'count',
-      cartTotal: 'total'
+      cartTotal: 'total',
+      shipping: 1500
     }),
+    subtotal() {
+      return this.cartTotal;
+    },
     total() {
-      // const temp= Math.max(this.cartTotal - this.promo.value, 0);
-      // console.log(this.cartTotal, this.promo.value, temp)
-      return Math.max(this.cartTotal - this.promo.value, 0);
+      if (this.subtotal > 0) {
+        return this.subtotal + 1500;
+      }
+      return 0;
+    },
+    createPedido() {
+      console.log(`nombre: ${this.nombre}, direccion: ${this.direccion}, ciudad: ${this.ciudad}, provincia: ${this.provincia}, pais: ${this.pais}`);
+      if (this.nombre && this.direccion && this.ciudad && this.provincia && this.pais) { 
+        this.$toast.success('Pedido creado con exito');
+        return 0;
+      }      
+      this.$toast.error("⚠️ Complete todos los campos para continuar ⚠️");
+      return false;
     }
+
   },
   data() {
+    
+
     return {
       currency: {
         name: 'CLP',
@@ -40,7 +59,12 @@ export default {
       promo: {
         code: 'EXAMPLECODE',
         value: 5000
-      }
+      },
+      nombre: '',
+      direccion: '',
+      ciudad: '',
+      provincia: '',
+      pais: 'Chile',
     };
   },
 };
